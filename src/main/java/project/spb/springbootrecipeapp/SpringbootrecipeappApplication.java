01021517bfe145a075baeb53bfd314530c2b157c
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-01-01 19:41:01 -0600

    Added Spring Boot project to the repository

---
+++ src/main/java/project/spb/springbootrecipeapp/SpringbootrecipeappApplication.java
@@ -0,0 +1,14 @@
+package project.spb.springbootrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrecipeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootrecipeappApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/project/spb/springbootrecipeapp/SpringbootrecipeappApplicationTests.java
@@ -0,0 +1,17 @@
+package project.spb.springbootrecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootrecipeappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
