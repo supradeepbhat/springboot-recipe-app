a4cc3b5432f9297dc5227c5b7eaa9fd8822fbdfe
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-01-06 14:07:00 -0600

    Added the one to many entity.

--- src/main/java/project/spb/springbootrecipeapp/domain/Recipe.java
+++ src/main/java/project/spb/springbootrecipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package project.spb.springbootrecipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -21,6 +22,9 @@ public class Recipe {
 
     @Lob
     private byte[] image;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -103,5 +107,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
