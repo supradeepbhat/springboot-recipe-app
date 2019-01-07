8f721fe830791a1e24c0ed8a295d5ca9e9f8e444
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-01-06 19:00:00 -0600

    Added the Many to Many JPA relationships for Recipe and Categories.

--- src/main/java/project/spb/springbootrecipeapp/domain/Recipe.java
+++ src/main/java/project/spb/springbootrecipeapp/domain/Recipe.java
@@ -27,6 +27,15 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns =  @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -113,5 +122,21 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
